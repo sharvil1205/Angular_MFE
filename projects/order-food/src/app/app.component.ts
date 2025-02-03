f7bc8a9af61d3ecab60fd44765dcc58c3cc9549c
Sharvil <79535235+sharvil1205@users.noreply.github.com>
2025-02-03 13:49:24 +0530

    Restaurant Listing page added

--- projects/order-food/src/app/app.component.ts
+++ projects/order-food/src/app/app.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatIconModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'order-food';
+  restaurants = [
+    {
+      restaurantID: 1,
+      name: 'KINGS BAKES',
+      cuisineType: 'VEG/NONVEG',
+      address: 'ODDANCHATRAM',
+      contactNo: '9865041255',
+      openingHours: '5AM - 12PM',
+    },
+    {
+      restaurantID: 2,
+      name: 'PIZZA WORLD',
+      cuisineType: 'VEG',
+      address: 'COIMBATORE',
+      contactNo: '9876543210',
+      openingHours: '10AM - 10PM',
+    },
+    {
+      restaurantID: 3,
+      name: 'THE BURGER HOUSE',
+      cuisineType: 'NONVEG',
+      address: 'CHENNAI',
+      contactNo: '9856741234',
+      openingHours: '11AM - 11PM',
+    },
+    {
+      restaurantID: 4,
+      name: 'SUSHI TIME',
+      cuisineType: 'VEG',
+      address: 'BANGALORE',
+      contactNo: '9823456789',
+      openingHours: '9AM - 9PM',
+    },
+  ];
 }
